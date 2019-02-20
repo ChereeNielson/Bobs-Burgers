275e2fab14b00dafb1eb55684a60dd1a60cbc84f
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-02-20 15:47:03 -0700

    html updates

--- models/burger.js
+++ models/burger.js
@@ -2,21 +2,25 @@
 let orm = require("../config/orm.js");
 
 let burger = {
+    // Display All Burgers in the DB //
     all: function(cb) {
         orm.all("burgers", function(response) {
             cb(response);
         });
     },
+    // Change the Devoured State to True //
     update: function(columnInput, vals, id, cb) {
         orm.update("burgers", columnInput, condition, function(res) {
             cb(res);
         });
     },
+    // Add a New Burger to the DB //
     create: function(cols, vals, cb) {
         orm.create("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
+    // Delete a Burger from the DB //
     delete: function(condition, cb) {
         orm.delete("burgers", condition, function(res) {
             cb(res);
