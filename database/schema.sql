11379f5978e540dea2760de39a6b61f0dbb88618
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-02-21 12:00:51 -0700

    fixed models

--- database/schema.sql
+++ database/schema.sql
@@ -9,3 +9,11 @@ CREATE TABLE burgers (
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
+
+
+-- Run the schema.sql and seeds.sql files into the MySQL server from the command line.
+-- Make sure you're in the db folder of your app.
+    -- 1. Start MySQL command line tool and login: <mysql -u root -p>.
+    -- 2. With the MySQL command line tool running, enter the command <source schema.sql>. This will run your schema file and all of the queries in it -- in other words, this creates your database.
+    -- 3. Now insert the entries you defined in seeds.sql by running the file: <source seeds.sql>.
+    -- 4. Close out of the MySQL command line tool: <exit>.
--- models/burger.js
+++ models/burger.js
@@ -3,26 +3,26 @@ let orm = require("../config/orm.js");
 
 let burger = {
     // Display All Burgers in the DB //
-    all: function(cb) {
-        orm.all("burgers", function(response) {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(response) {
             cb(response);
         });
     },
     // Change the Devoured State to True //
-    update: function(columnInput, vals, id, cb) {
-        orm.update("burgers", columnInput, condition, function(res) {
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
             cb(res);
         });
     },
     // Add a New Burger to the DB //
-    create: function(cols, vals, cb) {
-        orm.create("burgers", cols, vals, function(res) {
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
     // Delete a Burger from the DB //
-    delete: function(condition, cb) {
-        orm.delete("burgers", condition, function(res) {
+    deleteOne: function(condition, cb) {
+        orm.deleteOne("burgers", condition, function(res) {
             cb(res);
         });
     }
