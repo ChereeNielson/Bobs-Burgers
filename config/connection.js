ace4c81646e9fb31e1135c98862a7b97d22c045f
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-02-22 16:14:25 -0700

    added heroku connection

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,23 @@ const secret = require("../secret.js");
 
 // Setup MySQL Connection //
 let mysql = require("mysql");
-let connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: secret.pass.word,
-    database: "burgers_db"
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: secret.pass.word,
+        database: "burgers_db"
+    });
+};
 
 // Make Connection //
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
-    console.log("error connecting: " + err.stack);
-    return;
+        console.log("error connecting: " + err.stack);
+        return;
     }
     console.log("connected as id: " + connection.threadId);
 });
